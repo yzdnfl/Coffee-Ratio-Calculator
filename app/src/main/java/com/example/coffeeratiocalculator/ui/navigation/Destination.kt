9b33e6c929b90d28c089c9f78d50313a5d3963a4
yzdnfl <yazidnaufalvg@gmail.com>
2026-01-01 11:27:59 +0700

    header section update

--- app/src/main/java/com/example/coffeeratiocalculator/ui/navigation/Destination.kt
+++
@@ -1,9 +0,0 @@
-package com.example.coffeeratiocalculator.ui.navigation
-
-sealed class Destination(val route: String) {
-    object home: Destination("home")
-    object recipe: Destination("search")
-    object addRecipe: Destination("addRecipe")
-    object message: Destination("message")
-    object profile: Destination("profile")
-}
